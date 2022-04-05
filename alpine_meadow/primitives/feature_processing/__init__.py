b6c8f8c4713870612e9809d2aeb561fe67fa7fc9
Zeyuan Shang <zeyuanxy@gmail.com>
2022-04-05 02:07:33 -0400

    fix feature engineering

--- alpine_meadow/primitives/feature_processing/__init__.py
+++ alpine_meadow/primitives/feature_processing/__init__.py
@@ -1,4 +1,5 @@
 """ Alpine Meadow primitives for feature processing. """
 
-from .encoders import UnseenLabelEncoder, OneHotEncoder  # noqa: F401
 from .basic import ExtractColumnsByNames, HorizontalConcat, TimestampConverter  # noqa: F401
+from .encoders import UnseenLabelEncoder, OneHotEncoder  # noqa: F401
+from .rl import FeatureEngineering  # noqa: F401
